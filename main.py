360a5f7eff12973c71176f36fa739f25411b9745
Auri <f5bde6@proton.me>
2025-09-30 09:28:14 +0100

    Made type checker less angry and stream audio instead of downloading

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from dataclasses import dataclass
-from shutil import rmtree
-from tempfile import mkdtemp, mkstemp
 import discord
-from discord import Interaction
+from discord import Interaction, Member, VoiceChannel
 import os
-import uuid
 
 import yt_dlp
-from discord import  app_commands
+from discord import app_commands
+from discord.member import VocalGuildChannel
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,131 +15,140 @@ discord_guild = os.environ["DISCORD_GUILD"]
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+
 @dataclass
-class Voices:
-    client: discord.VoiceClient
+class State:
+    client: discord.VoiceClient | None = None
 
-def download_audio(id: str) -> str:
-    _, video_filename = mkstemp()
-    os.remove(video_filename)
 
-    ydl_opts = {
-        'outtmpl': f'{video_filename}',
-        'format': 'bestaudio/best',
-        'merge_output_format': 'opus',
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([f"https://www.youtube.com/watch?v={id}"])
+async def connect_to_voice(channel: VocalGuildChannel|None) -> bool:
+    if isinstance(channel, VoiceChannel):
+        connection = await channel.connect()
+        global state
+        state.client = connection
+        return True
+    else:
+        return False
 
-    return video_filename
-
-async def connect_to_voice(voice_id: int):
-    ch = await client.fetch_channel(voice_id)
-    connection = await ch.connect()
-
-    global state
-    state.client = connection
 
 global state
-state = Voices(None)
+state = State()
+
 
 @tree.command(
-    name="hello",
-    description="says hi",
-    guild=discord.Object(id=discord_guild)
+    name="hello", description="says hi", guild=discord.Object(id=discord_guild)
 )
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message("Hello!")
+    _ = await interaction.response.send_message("Hello!")
+
 
 @tree.command(
-    name="connect",
-    description="connects to vc",
-    guild=discord.Object(id=discord_guild)
+    name="connect", description="connects to vc", guild=discord.Object(id=discord_guild)
 )
 async def connect(interaction: discord.Interaction):
-    voice: discord.VoiceState = await interaction.user.fetch_voice()
+    if isinstance(interaction.user, Member)
+        voice: discord.VoiceState = await interaction.user.fetch_voice()
 
-    await connect_to_voice(voice.channel.id)
+        if await connect_to_voice(voice.channel):
+            _ = await interaction.response.send_message(
+                "Successfully connected to voice channel"
+            )
+        else:
+            _ = await interaction.response.send_message(
+                "Failed to connected to voice channel"
+            )
+    else:
+        print("Interaction user is not a insteanceof Member")
 
-    await interaction.response.send_message("Successfully connected to voice channel")
+def get_play_url(id: str)->str|None:
+    ydl_opts = {
+            'format': 'bestaudio/best',
+            'merge_output_format': 'opus',
+        }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info=ydl.extract_info(f"https://www.youtube.com/watch?v={id}")
+        return info.get("url")
 
 @tree.command(
-    name="play",
-    description="plays funny id",
-    guild=discord.Object(id=discord_guild)
+    name="play", description="plays funny id", guild=discord.Object(id=discord_guild)
 )
 async def play(interaction: Interaction, id: str):
-    voice: discord.VoiceState = await interaction.user.fetch_voice()
+    if not isinstance(interaction.user, Member) and state.client is None:
+        _ = await interaction.response.send_message(f"Failed to play {id}")
+    elif isinstance(interaction.user, Member) :
+        voice: discord.VoiceState = await interaction.user.fetch_voice()
 
-    if state.client is None:
-        await connect_to_voice(voice.channel.id)
-
+        if state.client is None:
+            if isinstance(voice, VocalGuildChannel):
+                if await connect_to_voice(voice.channel):
+                    _ = await interaction.response.send_message(
+                        "Successfully connected to voice channel"
+                    )
+                else:
+                    _ = await interaction.response.send_message(
+                        "Failed to connected to voice channel"
+                    )
+    assert state.client is not None
     state.client.stop()
-    await interaction.response.send_message(f"Trying to play {id}")
-    filename = download_audio(id)
-    source = discord.FFmpegOpusAudio(filename)
-    state.client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+    _ = await interaction.response.send_message(f"Trying to play {id}")
+    url=get_play_url(id)
+    if url is not None:
+        source = discord.FFmpegOpusAudio(url, options="-vn")
+        state.client.play(
+            source, after=lambda e: print(f"Player error: {e}") if e else None
+        )
+    else:
+         _ = await interaction.response.send_message(f"Invalid id: {id}")
+
 
 @tree.command(
-    name="resume",
-    description="Resumes",
-    guild=discord.Object(id=discord_guild)
+    name="resume", description="Resumes", guild=discord.Object(id=discord_guild)
 )
 async def resume(interaction: Interaction):
-    if state.client.is_playing():
-        await interaction.response.send_message("It is already playing")
-        return
+    if state.client is not None:
+        if state.client.is_playing():
+            _ = await interaction.response.send_message("It is already playing")
+            return
 
-    state.client.resume()
-    await interaction.response.send_message("Resumed")
+        state.client.resume()
+        _ = await interaction.response.send_message("Resumed")
+    else:
+        _ = await interaction.response.send_message("Bot is not in a VC")
+
 
 @tree.command(
-    name="pause",
-    description="pauses",
-    guild=discord.Object(id=discord_guild)
+    name="pause", description="pauses", guild=discord.Object(id=discord_guild)
 )
 async def pause(interaction: Interaction):
     if state.client is None or not state.client.is_playing():
-        await interaction.response.send_message("Nothing playing")
+        _ = await interaction.response.send_message("Nothing playing")
         return
 
     state.client.pause()
-    await interaction.response.send_message(f"Paused")
+    _ = await interaction.response.send_message(f"Paused")
 
-@tree.command(
-    name="stop",
-    description="Stops",
-    guild=discord.Object(id=discord_guild)
-)
+
+@tree.command(name="stop", description="Stops", guild=discord.Object(id=discord_guild))
 async def stop(interaction: Interaction):
     if state.client is None or not state.client.is_playing():
-        await interaction.response.send_message("Nothing playing")
+        _=await interaction.response.send_message("Nothing playing")
         return
 
     state.client.stop()
-    await interaction.response.send_message("Stopped")
+    _ = await interaction.response.send_message("Stopped")
 
-# @tree.command(
-#     name="hellnaw",
-#     description="hell naw",
-#     guild=discord.Object(id=discord_guild)
-# )
-# async def play(interaction: Interaction):
-#     interaction.message.author
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message: discord.message.Message):
     if message.author == client.user:
         return
 
+
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=discord_guild))
-    print ("Log : "+str(client.user))
+    _ = await tree.sync(guild=discord.Object(id=discord_guild))
+    print("Log : " + str(client.user))
+
 
 if __name__ == "__main__":
     client.run(os.environ["DISCORD_TOKEN"])
